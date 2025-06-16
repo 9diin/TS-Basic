e49ea15e13ae3d172c9a1bca1e1d8da3dc8201f5
9diin <weartstudio@naver.com>
2025-06-17 08:35:17 +0900

    5️⃣ 타입스크립트 - 배열

--- ch05/index.ts
+++ ch05/index.ts
@@ -1,18 +1,17 @@
 // 배열
 // 자바스크립트 배열은 매우 유연하고 내부에 모든 타입의 값을 혼합해서 저장할 수 있습니다.
-// 그러나 대부분의 개별 자바스크립트 배열은 하나의 특정 타입의 갓만 가집니다. 다른 타입의 값을 추가하게 되면 배열을 읽을 때 혼란을 줄 수 있으며,
+// 그러나 대부분의 개별 자바스크립트 배열은 하나의 특정 타입의 것만 가집니다. 다른 타입의 값을 추가하게 되면 배열을 읽을 때 혼란을 줄 수 있으며,
 // 최악의 상황으로는 프로그램에 문제가 될 만한 오류가 발생할 수도 있습니다.
 
 // 타입스크립트는 초기 배열에 어떤 데이터 타입이 있는지 기억하고, 배열이 해당 데이터 타입에서만 작동하도록 제한합니다.
 // 이런 방식으로 배열의 데이터 타입을 하나로 유지시킵니다.
 
-// 다음 예제에서 타입스크립트는 users 배열이 초기에 string 타입의 값을 포함한다는 것을 알고 있으므로 이후 string 타입의 값 추가는 허용하지만
+// 다음 예제에서 타입스크립트는 users 배열이 초기에 string 타입의 값을 포함한다는 것을 알고 있으므로, 이후 string 타입의 값 추가는 허용하지만
 // 다른 데이터 타입 추가는 허용하지 않습니다.
 
 const users = ["김아무개", "이아무개", "박아무개"];
-
 users.push("최아무개"); // OK: "최아무개"의 타입은 string
-users.push(true); // 'boolean' 형식의 인수는 'string' 형식의 매개 변수에 할당될 수 없습니다.
+users.push(true); // Error: 'boolean' 형식의 인수는 'string' 형식의 매개 변수에 할당될 수 없습니다.
 
 // 타입스크립트가 초기 배열에 담긴 요소를 통해 배열의 타입을 유추하는 방법은 변수의 초깃값에서 변수 타입을 유추하는 방법과 유사합니다.
 // 타입스크립트는 값이 할당되는 방식에서 코드의 의도된 타입을 이해하려고 시도하며 배열도 예외는 아닙니다.
@@ -24,7 +23,7 @@ arrayOfNumbers = [1, 2, 3, 4, 5];
 
 // 1.1 배열과 함수 타입
 // 배열 타입은 함수 타입에 무엇이 있는지를 구별하는 괄호가 필요한 구문 컨테이너의 예입니다.
-// 괄호는 애너테이션의 어느 부분이 함수 반환 부분이고 어느 부분이 배열 타입 묶음인지를 나타내기 위해 사용합니다.
+// 괄호는 애너테이션의 어느 부분이 함수 반환 부분이고 어느 부분이 배열 타입 묶음인지 나타내기 위해 사용됩니다.
 
 // 타입은 string 배열을 반환하는 함수
 let createStrings: () => string[];
@@ -39,23 +38,23 @@ let stringCreators: (() => string)[];
 let stringOrArrayOfNumbers: string | number[];
 
 // 타입은 각각 number 또는 string인 요소의 배열
-let arrayOfStringOrNumbers: (string | number)[];
+let arrayOfStringOfNumbers: (string | number)[];
 
-// 타입: (string | undefined)[]
-const optionalNames: (string | undefined)[] = ["김아무개", "이아무개", "박아무개", undefined];
+// 타입은 (string | undefined)[]
+const optionalNames: (string | undefined)[] = ["김아무개", "이아무개", undefined, "박아무개", undefined];
 
 // 1.3 any 배열의 진화
-// 초기에 빈 배열로 설정된 변수에서 타입 애너테이션을 포함하지 않으면 타입스크립트는 배열을 any[]로 취급하고 모든 콘텐츠를 받을 수 있습니다.
-// 하지만 any 변수가 변경되는 것처럼 any[] 배열이 변경되는 것도 좋지 않습니다.
+// 초기에 빈 배열로 설정된 변수에서 타입 애너테이션을 포함하지 않으면 타입스크립트는 배열을 any[]로 취급하고, 모든 콘텐츠를 받을 수 있습니다.
+// 하지만 any 변수가 변경되는 것처럼 any[] 배열이 변경되는 것은 좋지 않습니다.
 // 타입 애너테이션이 없는 빈 배열은 잠재적으로 잘못된 값 추가를 허용해 타입스크립트의 타입 검사기가 갖는 이점을 부분적으로 무력화 시키기 때문입니다.
 
-// 다음 예제는 values 배열은 any 요소를 갖고 시작해 string 요소를 포함하도록 바꾼 다음, 다시 number | string 요소로 바뀝니다.
+// 다음 예제는 values 배열은 any 요소를 갖고 시작해 string 요소를 포함하도록 바꾼 다음, 다음 number | string 요소로 바꿉니다.
 
 // 타입: any[]
 let values = [];
 
 // 타입: string[]
-values.push("");
+values.push("김아무개");
 
 // 타입: (number | string)[]
 values[0] = 0;
@@ -79,19 +78,20 @@ arrayOfArrayOfNumbers = [
 // 유니언 타입으로 된 배열의 멤버는 그 자체로 동일한 유니언 타입입니다.
 
 const defenders = ["김아무개", "이아무개", "박아무개"];
-const defender = defenders[0]; // 타입 string
+const defender = defenders[0]; // 타입: string
 
-const soldiersOrDates = ["김아무개", new Date(1782, 6, 3)];
-const soldierOrDate = soldiersOrDates[0]; // 타입 string | Date
+const soldierOrDates = ["김아무개", new Date(1782, 6, 3)];
+const soldierOrDate = soldierOrDates[0]; // 타입: string | Date
 
 // 3. 스프레드와 나머지 매개변수
 // 3.1 스프레드
 // ... 스프레드 연산자를 사용해 배열을 결합합니다. 타입스크립트는 입력된 배열 중 하나의 값이 결과 배열에 포함될 것임을 이해합니다.
-// 입력된 배열이 동일한 타입이라면, 출력 배열도 동일한 타입입니다. 서로 다른 타입의 두 배열을 함께 스프레드해 새 배열을 생성하면 새 배열은 두 개의 원래 타입 중 어느 하나의 요소인 유니언 타입 배열로 이해됩니다.
+// 입력된 배열이 동일한 타입이라면, 출력 배열도 동일한 타입입니다.
+// 서로 다른 타입의 두 배열을 함께 스프레드에 새 배열을 생성하면, 새 배열은 두 개의 원래 타입 중 어느 하나의 요소인 유니언 타입 배열로 이해합니다.
 
-const soldiers = ["김아무개", "이아무개", "박아무개"]; // 타입 string[]
-const soldierAges = [10, 20, 30]; // 타입 number[]
-const conjoined = [...soldierAges, ...soldierAges]; // 타입 (string | number)[]
+const soldiers = ["김아무개", "이아무개", "박아무개"]; // 타입: string[]
+const soldierAges = [10, 20, 30]; // 타입: number[]
+const conjoined = [...soldiers, ...soldierAges]; // 타입: (string | number)[]
 
 // 3.2 나머지 매개변수 스프레드
 // 타입스크립트는 나머지 매개변수로 배열을 스프레드하는 자바스크립트 실행을 인식하고 이에 대해 타입 검사를 수행합니다.
@@ -103,10 +103,10 @@ function logWarriors(greeting: string, ...names: string[]) {
     }
 }
 const warriors = ["김아무개", "이아무개", "박아무개"];
-const birthYears = [1844, 1840, 1583];
+const birthYears = [1844, 1840, 1592];
 
 logWarriors("Hello", ...warriors);
-logWarriors("Born in", birthYears); // Error: 'number[]' 형식의 인수는 'string' 형식의 매개 변수에 할당될 수 없습니다.
+logWarriors("Born in", ...birthYears); // Error: 'number' 형식의 인수는 'string' 형식의 매개 변수에 할당될 수 없습니다.
 
 // 4. 튜플
 // 자바스크립트 배열은 이론상 어떤 크기라도 될 수 있습니다.
