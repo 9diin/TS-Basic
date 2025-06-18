6da082ec92c19b07b345cf1b84c677c8504cf683
9diin <weartstudio@naver.com>
2025-06-18 19:18:45 +0900

    5️⃣ 타입스크립트 - 배열

--- ch05/index.ts
+++ ch05/index.ts
@@ -110,17 +110,18 @@ logWarriors("Born in", ...birthYears); // Error: 'number' 형식의 인수는 's
 
 // 4. 튜플
 // 자바스크립트 배열은 이론상 어떤 크기라도 될 수 있습니다.
-// 하지만 때로는 튜플Tuple이라고 하는 고정된 크기의 배열을 사용하는 것이 유용합니다.
-// 튜플 배열은 각 인덱스에 알려진 특징 타입을 가지며 배열의 모든 가능한 멤버를 갖는 유니언 타입보다 더 구체적입니다.
-// 튜플 타입을 선언하는 구문은 배열 리터럴처럼 보이지만 요소의 값 대신 타입을 적습니다.
+// 하지만 때로는 튜플 Tuple이라고 하는 고정된 크기의 배열을 사용하는 것이 유용합니다.
+// 튜플 배열은 각 인덱스에 알려진 특정 타입을 가지며, 배열의 모든 가능한 멤버를 갖는 유니언 타입보다 더 구체적입니다.
+// 튜플 타입을 선언하는 구문은 배열 리터럴처럼 보이지만, 요소의 값 대신 타입을 적습니다.
 
 let yearAndWarrior: [number, string];
 yearAndWarrior = [530, "김아무개"]; // OK
-yearAndWarrior = [false, "이아무개"]; // 'boolean' 형식은 'number' 형식에 할당할 수 없습니다.
-yearAndWarrior = [1000]; // Error: '[number]' 형식은 '[number, string]' 형식에 할당할 수 없습니다. 소스에 1개 요소가 있지만, 대상에 2개가 필요합니다.
+yearAndWarrior = [false, "이아무개"]; // Error: 'boolean' 형식은 'number' 형식에 할당할 수 없습니다.
+yearAndWarrior = [1000]; // Error: '[number]' 형식은 '[number, string]' 형식에 할당할 수 없습니다.
 
-// 자바스크립트에서는 단일 조건을 기반으로 두 개의 변수에 초깃값을 설정하는 것처럼 한 번에 여러 값을 할당하기 위해 튜플과 배열 구조 분해 할당을 함께 자주 사용합니다.
-let [year, warrior] = Math.random() > 0.5 ? [340, "김아무개"] : [1828, "박아무개"];
+// 자바스크립트에서는 단일 조건을 기반으로 두 개의 변수에 초깃값을 설정하는 것처럼 한 번에 여러 값을 할당하기 위해 튜플과 배열 구조 분해 할당을 자주 사용합니다.
+let [year, warrior] = Math.random() > 0.5 ? ["340", "김아무개"] : [1828, "이아무개"];
+// let [username, email] = user  ? [user.username, user.email] : ["알 수 없음", "미등록"];
 
 // 4.1 튜플 할당 가능성
 // 타입스크립트에서 튜플 타입은 가변 길이의 배열 타입보다 더 구체적으로 처리됩니다. 즉, 가변 길이의 배열 타입은 튜플 타입에 할당할 수 없습니다.
@@ -140,19 +141,20 @@ const tupleTwoExtra: [boolean, number] = tupleThree; // Error: '[boolean, number
 
 // 4.2 나머지 매개변수로서의 튜플
 function logPair(name: string, value: number) {
-    console.log(`${name} hase ${value}`);
+    console.log(`${name} has ${value}`);
 }
 const pairArray = ["Amage", 1]; // 타입: (string | number)[]
 logPair(...pairArray); // Error: 확산 인수는 튜플 유형을 가지거나 나머지 매개 변수로 전달되어야 합니다.
 
-const pairTupleIncorrect: [number, string] = [1, "Image"];
-logPair(...pairTupleIncorrect); // Error: 'number' 형식의 인수는 'string' 형식의 매개 변수에 할당될 수 없습니다.
+const pairTupleIncorrect: [number, string] = [1, "Amage"];
+logPair(...pairTupleIncorrect); // 'number' 형식의 인수는 'string' 형식의 매개 변수에 할당될 수 없습니다.
 
-const pairTupleCorrect: [string, number] = ["Image", 1];
+const pairTupleCorrect: [string, number] = ["Amage", 1];
 logPair(...pairTupleCorrect);
 
 // 4.3 튜플 추론
-// 타입스크립트는 생성된 배열을 튜플이 아닌 가변 길이의 배열로 취급합니다. 배열이 변수의 초깃값 또는 함수에 대한 반환값으로 사용되는 경우, 고정된 크기의 튜플이 아니라 유연하나 크기의 배열로 가정합니다.
+// 타입스크립트는 생성된 배열을 튜플이 아닌 가변 길이의 배열로 취급합니다. 배열이 변수의 초깃값 또는 함수에 대한 반환값으로 사용되는 경우,
+// 고정된 크기의 튜플이 아니라 유연한 크기의 배열로 가정합니다.
 
 function firstCharAndSize(input: string) {
     return [input[0], input.length]; // 반환 타입: (string | number)[]
@@ -169,7 +171,7 @@ function firstCharAndSizeExplicit(input: string): [string, number] {
 const [char, charSize] = firstCharAndSizeExplicit("안녕하세요");
 
 // 4.3.2 const 어서션
-// 명시적 타입 애너테이션에 튜플 타입을 입력하는 작업은 명시적 타입 애너테이션을 입력하라 때와 동일한 이유로 번거로울 수 있습니다.
+// 명시적 타입 애너테이션에 튜플 타입을 입력하는 작업은 명시적 타입 애너테이션을 입력하는 때와 동일한 이유로 번거로울 수 있습니다.
 // 대안으로 타입스크립트는 값 뒤에 넣을 수 있는 const 어서션인 as const 연산자를 제공합니다.
 // const 어서션은 타입스크립트에 타입을 유추할 때 읽기 전용이 가능한 값 형식을 사용하도록 지시합니다.
 // 다음과 같이 배열 리터럴 뒤에 as const가 배치되면 배열이 튜플로 처리되어야 함을 나타냅니다.
